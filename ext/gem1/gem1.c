b561f59cb710a289f55d1759648e92a7e29a6b76
Vishal Chandnani <vschandnani@gmail.com>
2014-07-06 17:13:26 -0400

    [VC] C Extension for Ruby: "Hello Ruby from C!" Gem
    - Removed rb_mGem1

--- ext/gem1/gem1.c
+++ ext/gem1/gem1.c
@@ -1,8 +1,6 @@
 #include "gem1.h"
 #include <ruby.h>
 
-// VALUE rb_mGem1;
-
 static VALUE object_first_cext_call(VALUE rb_self)
 {
   printf("\n Hello Ruby from C! \n");
@@ -11,6 +9,5 @@ static VALUE object_first_cext_call(VALUE rb_self)
 void
 Init_gem1(void)
 {
-  // rb_mGem1 = rb_define_module("Gem1");
   rb_define_method(rb_cObject, "first_cext_call", object_first_cext_call, 0);
 }
